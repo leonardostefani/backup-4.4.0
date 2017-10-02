7499eed97116b54528f446c0306f1bdedad7bf5c
vtm <vtmilyakov@yandex.ru>
2017-10-02 23:48:51 +0300

    Fix cycling in  dropbox api v2

--- lib/backup/storage/dropbox.rb
+++ lib/backup/storage/dropbox.rb
@@ -97,7 +97,7 @@ module Backup
       def remove!(package)
         Logger.info "Removing backup package dated #{ package.time }..."
 
-        client.delete(remote_path_for(package))
+        client.delete("/#{remote_path_for(package)}")
       end
     end
   end
